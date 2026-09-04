a5d3200ca480dc2a95c29f70b0bf82b22c8189fa
Harshada <harshadataur@gmail.com>
2026-09-04 13:11:26 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        // Jenkins runs non-interactively, so we use a default value instead of Scanner
+        String name = "DevOps Engineer";
         
         System.out.println("Hello, " + name + "! Welcome to Java.");
-        
-        scanner.close();
     }
 }
